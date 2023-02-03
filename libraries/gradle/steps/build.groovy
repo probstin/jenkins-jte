f45f5054a834a5d9e75e6e5ecf870c8b2ada7cbf
Justin Probst <41829981+probstin@users.noreply.github.com>
2023-02-03 10:59:42 -0500

    test a container agent

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -1,5 +1,27 @@
 void call(){
-    stage("Gradle: Build"){ 
+    agent {
+        kubernetes {
+          yaml '''
+            apiVersion: v1
+            kind: Pod
+            spec:
+              containers:
+              - name: gradle
+                image: gradle:jdk17-alpine
+                command:
+                - cat
+                tty: true
+            '''
+        }
+    }
+    
+    stage('Gradle:Build') {
         println "build from the gradle library" 
+
+        steps {
+            container('gradle') {
+              sh 'gradle -version'
+            }
+        }
     }
 }
