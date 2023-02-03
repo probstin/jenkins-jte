b22f68a5820db85c62256d2b38f6afe46ddf97f0
Justin Probst <41829981+probstin@users.noreply.github.com>
2023-02-03 11:25:29 -0500

    Delete splunk_step_watcher.groovy

--- libraries/splunk/splunk_step_watcher.groovy
+++
@@ -1,4 +0,0 @@
-@AfterStep({ config.afterSteps ? (context.step in config.afterSteps) : true })
-void after(context) {
-  println "Splunk: running after the ${context.library} library's ${context.step} step"
-}
