1e797d7b07a9e66f9fbe2eb70b3d9b2ec9492ce2
Justin Probst <41829981+probstin@users.noreply.github.com>
2023-02-05 16:47:57 -0500

    Update build.groovy

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -2,12 +2,14 @@ void call() {
     
     String imageTag = config.image_tag ?: "jdk17-alpine"
     
-    podTemplate(containers: [containerTemplate(name: 'gradle', image: "gradle:${imageTag}", command: 'sleep', args: '99d')]) {
+    podTemplate(
+        containers: [containerTemplate(name: 'gradle', image: "gradle:${imageTag}", command: 'sleep', args: '99d')],
+    ) {
         node(POD_LABEL) {
             stage('Gradle:Build') {
                 container('gradle') {
                     sh 'gradle -version'
-                    sh 'ls -altr'
+                    sh 'ls /home/jenkins/agent'
                 }
             }
         }  
