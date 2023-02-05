1afb0e7baac30b03b52f38f4dbaad6d7fc6cd647
Justin Probst <41829981+probstin@users.noreply.github.com>
2023-02-05 17:06:54 -0500

    Update build.groovy

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -1,11 +1,16 @@
 void call() {
     
-    String imageTag = config.image_tag ?: "jdk17-alpine"
+    def imageTag = config.image_tag ?: "jdk17-alpine"
     
     podTemplate(
         containers: [containerTemplate(name: 'gradle', image: "gradle:${imageTag}", command: 'sleep', args: '99d', ttyEnabled: true)],
     ) {
         node(POD_LABEL) {
+            
+            def myRepo = checkout scm
+            def gitCommit = myRepo.GIT_COMMIT
+            def gitBranch = myRepo.GIT_BRANCH
+            
             stage('Gradle:Build') {
                 container('gradle') {
                     sh """
