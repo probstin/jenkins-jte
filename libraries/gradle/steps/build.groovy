264144e127f6d7de51b4ee0e5a9fa07fd70cfe85
Justin Probst <41829981+probstin@users.noreply.github.com>
2023-02-05 17:12:14 -0500

    Update build.groovy

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -1,11 +1,14 @@
 void call() {
     
+    def label = "worker-${UUID.randomUUID().toString()}"
     def imageTag = config.image_tag ?: "jdk17-alpine"
     
     podTemplate(
+        label: label,
         containers: [containerTemplate(name: 'gradle', image: "gradle:${imageTag}", command: 'sleep', args: '99d', ttyEnabled: true)],
+        volumes: [hostPathVolume(mountPath: '/home/gradle/.gradle', hostPath: '/tmp/jenkins/.gradle')]
     ) {
-        node(POD_LABEL) {
+        node(label) {
             
             def myRepo = checkout scm
             def gitCommit = myRepo.GIT_COMMIT
